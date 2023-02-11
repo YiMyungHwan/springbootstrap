9b375e5a5bf988770971a12b2d86689f906c5f8e
YiMyungHwan <maxxxan@daum.net>
2023-02-11 21:14:20 +0900

    bootstrap 파일을 static 위치로 복사

--- src/main/java/com/mx/springbootstrap/controller/Hello.java
+++ src/main/java/com/mx/springbootstrap/controller/Hello.java
@@ -1,5 +1,6 @@
 package com.mx.springbootstrap.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Hello {
+    @Value("${spring.mvc.static-path-pattern}")
+    String resourcesPath;
+
     @GetMapping("/test")
     public String test() {
+
+        System.out.println(resourcesPath);
+
         return "Hello world";
     }
 }
