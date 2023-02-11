82a0f5c5a009b849837e6f503701b0b7c12ab720
YiMyungHwan <maxxxan@daum.net>
2023-02-11 19:15:38 +0900

    bootstrap 을 springboot 에서 사용하기위해 directory 추가

---
+++ src/main/java/com/mx/springbootstrap/controller/BootstrapController.java
@@ -0,0 +1,12 @@
+package com.mx.springbootstrap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BootstrapController {
+    @RequestMapping("/dashboard")
+    public String dashboard() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/mx/springbootstrap/SpringbootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.springbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
